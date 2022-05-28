b1917c79324b8080b7cd88da686e9a5d62520d44
carterlan <carteronelah@gmail.com>
2022-05-28 17:53:42 -0600

    update6_1

---
+++ chamber/js/windchill.js
@@ -0,0 +1,17 @@
+//Inputs for calculation of wind chill
+const temp = document.querySelector('#temp')
+const windspeed = document.querySelector('#windSpeed')
+
+let temp1 = parseFloat(temp.textContent)
+let windspeed1 = parseFloat(windspeed.textContent)
+
+if (temp1 <= 50 && windspeed1 >= 3.0) {
+    let windchill = 35.74 + 0.6215 * temp1 - 35.75 * windspeed1 ** (0.16) + 0.4275 * temp1 * windspeed1 ** (0.16);
+    windchillformated = windchill.toFixed(0);
+    document.querySelector('#windChill').innerHTML = `${windchillformated}°F`;
+} else {
+    let windchill = "N/A";
+    document.querySelector('#windChill').innerHTML = `${windchill}`;
+}
+
+
