c5ebf781a5493fe51872ff0f5e90fb17309e9d94
carterlan <carteronelah@gmail.com>
2022-06-18 13:01:24 -0600

    update9_9

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -55,3 +55,24 @@ fetch(requestURL)
     const members = jsonObject['members'];
     members.forEach(displayMembers);
 });
+
+//Grid versus List Action
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector("article");
+
+// The following code could be written cleaner. How? We may have to simplfiy our HTMl and think about a default view.
+
+gridbutton.addEventListener("click", () => {
+	// example using arrow function
+	display.classList.add("grid");
+	display.classList.remove("list");
+});
+
+listbutton.addEventListener("click", showList); // example using defined function
+
+function showList() {
+	display.classList.add("list");
+	display.classList.remove("grid");
+
+}
