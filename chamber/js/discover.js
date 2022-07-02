ab28d3f3295baeb72ca49d350c3b1dc9d27f5813
carterlan <carteronelah@gmail.com>
2022-07-02 13:13:57 -0600

    update11_3

--- chamber/js/discover.js
+++ chamber/js/discover.js
@@ -5,13 +5,9 @@ const cards = document.querySelector('.cards');
 function displayMembers(member) {
     // Create elements to add to the document
     let card = document.createElement('section');
-    let name = document.createElement('h2');
-    let address = document.createElement('h4');
-    let phone = document.createElement('h4');
-    let email = document.createElement('h4');
-    let website = document.createElement('a');
-    let memberlevel = document.createElement('h2');
-    let logo = document.createElement('img');
+    let race = document.createElement('h2');
+    let population= document.createElement('h4');
+    let percentage = document.createElement('h4');
 
     // Website creator
     //let linktext = document.createTextNode(`${member.name}`);
@@ -20,26 +16,17 @@ function displayMembers(member) {
     //a.href = `${member.website}`;
 
     // Change the textContent property to contain the member's full name, address, phone, email
-    name.textContent = `${member.name}`;
-    address.textContent = `Address: ${member.address}`
-    phone.textContent = `Phone Number: ${member.phone}`
-    email.textContent = `Email: ${member.email}`
-    memberlevel.textContent = `${member.memberlevel} Member`
-    website.href= member.website
-    website.textContent =(`${member.name}`)
-    // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
-    logo.setAttribute('src', member.imageurl);
-    logo.setAttribute('alt', `Logo of ${member.name}`);
-    logo.setAttribute('loading', 'lazy');
+    race.textContent = `${member.Race}`;
+    population.textContent = `Address: ${member.Population}`
+    percentage.textContent = `Phone Number: ${member.Percentage}`
+   
+
   
     // Add/append the section(card) with the h2 element
-    card.appendChild(name);
-    card.appendChild(address);
-    card.appendChild(phone);
-    card.appendChild(email);
-    card.appendChild(website);
-    card.appendChild(memberlevel);
-    card.appendChild(logo);
+    card.appendChild(race);
+    card.appendChild(population);
+    card.appendChild(percentage);
+  
   
     // Add/append the existing HTML div with the cards class with the section(card)
     document.querySelector('div.cards').appendChild(card);
@@ -55,23 +42,6 @@ fetch(requestURL)
     members.forEach(displayMembers);
 });
 
-//Grid versus List Action
-const gridbutton = document.querySelector("#grid");
-const listbutton = document.querySelector("#list");
+
 const display = document.querySelector("article");
 
-// The following code could be written cleaner. How? We may have to simplfiy our HTMl and think about a default view.
-
-gridbutton.addEventListener("click", () => {
-	// example using arrow function
-	display.classList.add("grid");
-	display.classList.remove("list");
-});
-
-listbutton.addEventListener("click", showList); // example using defined function
-
-function showList() {
-	display.classList.add("list");
-	display.classList.remove("grid");
-
-}
