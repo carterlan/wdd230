784f76091b0256d7d64d10be6aa6101677900046
carterlan <carteronelah@gmail.com>
2022-07-02 19:56:22 -0600

    Update11_10

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -60,7 +60,6 @@ const gridbutton = document.querySelector("#grid");
 const listbutton = document.querySelector("#list");
 const display = document.querySelector("article");
 
-// The following code could be written cleaner. How? We may have to simplfiy our HTMl and think about a default view.
 
 gridbutton.addEventListener("click", () => {
 	// example using arrow function
