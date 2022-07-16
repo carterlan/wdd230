f59d72bbf303daf0c76a3d33de7825bcbf57b060
carterlan <carteronelah@gmail.com>
2022-07-16 12:08:41 -0600

    Update1

---
+++ Temple_Inn_Suites/js/index.js
@@ -0,0 +1,26 @@
+//Dates for the Home page
+const currentDate = new Date();
+const year = currentDate.getFullYear();
+
+const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+const today = new Date().toLocaleDateString('en-UK', options);
+
+//document.querySelector('#today').textContent = today;
+
+document.querySelector('#lastUpdated').textContent = `Last Modification: ${document.lastModified}`;
+
+document.querySelector('#copyRight').innerHTML = `\u00A9 ${year} Temple Inn & Suites <br> <b>Created By: Landon Carter</b> <br>`;
+
+//Interactive menu
+const hambutton = document.querySelector('.ham');
+const mainnav = document.querySelector('.navigation')
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+// To solve the mid resizing issue with responsive class on
+window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+
+
+//Hidden date submit for Join page form
+dateTime = document.getElementById("dateTime");
+dateTime = currentDate;
