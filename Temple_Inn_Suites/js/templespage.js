fb507dd5e68b39990634f1260c2e2aa4718b111b
carterlan <carteronelah@gmail.com>
2022-07-18 17:20:34 -0600

    json temples

--- Temple_Inn_Suites/js/templespage.js
+++ Temple_Inn_Suites/js/templespage.js
@@ -17,6 +17,7 @@ function displaytemples(temple) {
     portrait.setAttribute('src', temple.src);
     portrait.setAttribute('alt', `Picture of ${temple.name} temple`);
     portrait.setAttribute('loading', 'lazy');
+    portrait.setAttribute('width', '100%')
   
     // Add/append the section(card) with the h2 element
     card.appendChild(h2);
